b0641dceaeac24c55a99f05c2c326b0ef0d6594c
Edson Junior <edsonjos61@gmail.com>
2022-05-03 11:28:52 -0300

    Refatorando o código. E habilitando e desabilitando botão.

--- src/components/FormularioPrincipal.vue
+++ src/components/FormularioPrincipal.vue
@@ -5,23 +5,7 @@
                 <input type="text" class="input" placeholder="Qual tarefa você deseja iniciar? ">
             </div>
             <div class="column">
-                <div class="is-flex is-align-items-center is-justify-content-space-between">
-                    
-                    <CronometroPrincipal :tempoEmSegundos="tempoEmSegundos"/>
-                    
-                    <button class="button" @click="iniciar">
-                        <span class="icon">
-                            <i class="fas fa-play"></i>
-                        </span>
-                        <span>play</span>
-                    </button>
-                    <button class="button" @click="finalizar">
-                        <span class="icon">
-                            <i class="fas fa-stop"></i>
-                        </span>
-                        <span>stop</span>
-                    </button>
-                </div>
+                <TemporizadorPrincipal />
             </div>
         </div>
     </div>
@@ -30,7 +14,7 @@
 <script lang="ts">
 
 import { defineComponent } from 'vue'
-import CronometroPrincipal from './CronometroPrincipal.vue'
+import TemporizadorPrincipal from './TemporizadorPrincipal.vue'
 
 
 export default defineComponent ({
@@ -38,27 +22,8 @@ export default defineComponent ({
     name: 'FormularioPrincipal',
 
     components: { 
-        CronometroPrincipal
+        TemporizadorPrincipal
     },
-
-    data(){
-        return {
-            tempoEmSegundos: 0,
-            cronometro : 0
-        }
-    },
-
-    methods:{
-        iniciar(){
-            this.cronometro = setInterval(() => {
-                this.tempoEmSegundos += 1
-            } , 1000)
-           
-        },
-        finalizar(){
-            clearInterval(this.cronometro);
-        }
-    }
 })
 </script>
 
